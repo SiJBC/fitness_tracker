853e98512ff70ca59ed93f7b45f626fa245e91df
Simon Colman <s_colman@hotmail.com>
2020-03-21 14:56:35 +1100

    heroku deploy

--- models/workout.js
+++ models/workout.js
@@ -1,63 +1,59 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-
+const workoutSchema = new Schema(
+  {
     day: {
-        type: Date,
-        default: () => new Date()
+      type: Date,
+      default: Date.now
     },
-
-    exercises: [{
+    exercises: [
+      {
         type: {
-            type: String,
-            trim: true,
-            required: "Enter an exercise type"
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
         },
         name: {
-            type: String,
-            trim: true,
-            required: "Enter an exercise name"
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
         },
-
         duration: {
-            type: Number,
-            required: "Enter a duration for the exercise"
+          type: Number,
+          required: "Enter an exercise duration in minutes"
         },
-
         weight: {
-            type: Number
+          type: Number
         },
-
         reps: {
-            tpe: Number
+          type: Number
         },
-
         sets: {
-            type: Number
+          type: Number
         },
-
         distance: {
-            type: Number
+          type: Number
         }
-    }
+      }
     ]
-},
-    {
-        toJSON: {
-            // include any virtual properties when data is requested
-            virtuals: true
-        }
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
     }
+  }
 );
 
 // adds a dynamically-created property to schema
-workoutSchema.virtual("totalDuration").get(function () {
-    // "reduce" array of exercises down to just the sum of their durations
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
-})
+workoutSchema.virtual("totalDuration").get(function() {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-const express = require("express")
-const logger = require("morgan")
-const mongoose = require("mongoose")
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
-// const db = require("./models");
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", 
-{ useNewUrlParser: true,
-  useFindAndModify: false });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb: //localhost/mongoHeadlines";
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-require("./routes/html-route.js")(app);
+const db = require("./models");
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
